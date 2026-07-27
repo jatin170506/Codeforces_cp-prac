930e8b4c8891be1f57e1a9397878a7d2338c6cdc
jatin170506 <jbhandari287@gmail.com>
2026-07-27 10:36:08 +0530

    Day-14, 1794B - Not Dividing

---
+++ Day-14/Not_dividing.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Not_dividing{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            for (int i = 0; i < n; i++) {
+                if (arr[i] == 1) {
+                    arr[i]++;
+                }
+            }
+            for (int i = 1; i < n; i++) {
+                if (arr[i] % arr[i - 1] == 0) {
+                    arr[i]++;
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i]);
+                if (i < n - 1) 
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
