4c96fe60f06cd69f252e3434d78508cf7e592bd1
jatin170506 <jbhandari287@gmail.com>
2026-08-15 20:34:41 +0530

    Day-33, 1295A - Display The Number

---
+++ Day-33/Display_num.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Display_num {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            if (n % 2 == 1) {
+                System.out.print('7');
+                n -= 3;
+            }
+            while (n > 0) {
+                System.out.print('1');
+                n -= 2;
+            }
+            System.out.println();
+        }
+    }
+}
