290a2dc58002baf8259e81231d067a262c4302c3
jatin170506 <jbhandari287@gmail.com>
2026-09-04 19:48:00 +0530

    Day-53, 1269A - Equation

---
+++ Day-53/Eqn.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Eqn{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextLong();
+        
+        long a = 9*n;
+        long b = 8*n;
+        
+        System.out.println(a + " " + b);
+    }
+}
