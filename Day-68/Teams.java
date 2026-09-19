173ea72f77eb8737b3aa901a28e119b2d11ba222
jatin170506 <jbhandari287@gmail.com>
2026-09-19 20:01:01 +0530

    Day-68, 1092B - Teams Forming

---
+++ Day-68/Teams.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Teams{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(Sytem.in);
+        
+        int n = sc.nextInt();
+        
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        
+        Arrays.sort(a);
+        int totalProblems = 0;
+        for (int i = 0; i < n; i += 2)
+            totalProblems += a[i + 1] - a[i];
+            
+        System.out.println(totalProblems);
+    }
+}
